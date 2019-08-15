28cabaa6ce68395a436500790f4df79ec1f68f77
DE0414\User <aslandukov-matvej@yandex.ua>
2019-08-15 19:33:38 +0300

    Fixed bug with start button

--- created_tables/scripts/filter_regions.js
+++ created_tables/scripts/filter_regions.js
@@ -142,6 +142,7 @@ function updateEnable(index, elem) {
 function disableRegions(disabled) {
     to_update = disabled;
     $(".row_region").each(updateEnable);
+    document.getElementsByTagName('button')[0].disabled = to_update;
 }
 
 function updateRegionstate(index, elem) {
