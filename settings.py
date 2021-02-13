21bb1f1664e5b1ba2998ecfe70052e903580593b
AsMaNick <aslandukov-matvej@yandex.ua>
2021-02-13 20:54:20 +0200

    Added itmo rating

--- settings.py
+++ settings.py
@@ -1,34 +1,38 @@
-﻿statistic_team_number = 5
+﻿statistic_team_number = 10
 path_to_scripts = '../../'
-ignore_regions = {'School'}
+ignore_regions = {'Default'}
 show_regions = True
-olympiad_title = 'The summer programming school, Uzhgorod-2019'
-olympiad_date = '3<sup>rd</sup> tour, August 11, 2019'
+olympiad_title = 'All-Ukrainian Collegiate Programming Contest'
+olympiad_date = '1<sup>st</sup> Stage Ukraine, September 26, 2020'
 links = [('http://ejudge.khai.edu/ejudge/contest180421.html', 'East')]
 
 round_time = 'UP'
 contest_duration = 300
-problems = 11
+problems = 12
 problem_names = [
-    'Школа танців',
-    'Розбиття кола',
-    'Сума степенів',
-    'Фарбування паркану',
-    'Продовжіть послідовність',
-    'Непарні підпрямокутники',
-    'Гарні дерева',
-    'Ще одна сума (easy)',
-    'Ще одна сума (hard)',
-    'Намисто',
-    'Підрахунок різних доданків'
+    'Знищiть масив',
+    'Максимальний НСД',
+    'Напiй',
+    'Мiнiмальна функцiя',
+    'ABC',
+    'Кiстякове дерево',
+    'Дев’ять',
+    'Зменшити масив',
+    'Вулиця',
+    'Однаковi цифри',
+    'Кульки',
+    'Трiйки'
 ]
-problem_ids = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H1', 'H2', 'I', 'J']
+problem_ids = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
 
 penalty_points = 20
-ignore_compilation_error = False
-csv_files = ['day3.csv']
-regions = ['Ukraine']
-path_to_data = 'data/uzhgorod/'
-path_to_team_members = 'data/uzhgorod/team_members.txt' # empty string for ignore
+ignore_compilation_error = True
+csv_files = ['center.csv', 'east.csv', 'north.csv', 'south.csv', 'sw.csv', 'west.csv']
+regions = ['Center', 'East', 'North', 'South', 'South-West', 'West']
+path_to_data = 'data/2020_1/'
+path_to_team_regions = '' # empty string for ignore
+path_to_team_members = '' # empty string for ignore
 path_to_unofficial_teams = '' # empty string for ignore
-standings_file_name = 'uzhgorod_3'
+standings_file_name = 'standings'
+write_team_members = True
+show_itmo_rating = False
