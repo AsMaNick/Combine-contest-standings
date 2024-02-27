534afa752d1427277b55515b2ed07a9aa9782fae
Matvey Aslandukov <matvej909@gmail.com>
2024-02-27 14:02:59 +0100

    Improved footer to automatically calculate date

--- created_tables/scripts/footer.js
+++ created_tables/scripts/footer.js
@@ -1,3 +1,3 @@
 function loadFooter() {
-    document.getElementById("footer").innerHTML = `Copyright © 2017-2022 <a href="https://t.me/Matvejj">Matvii Aslandukov</a>. All rights reserved.`;
+    document.getElementById("footer").innerHTML = `Copyright © 2017-${new Date().getFullYear()} <a href="https://t.me/Matvejj">Matvii Aslandukov</a>. All rights reserved.`;
 }
