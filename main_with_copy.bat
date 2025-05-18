dc43984410da6ad56c114fbce1c83f6d1faa10e5
Matvey Aslandukov <matvej909@gmail.com>
2025-05-18 15:12:30 +0200

    Updated to latest stage

--- main_with_copy.bat
+++ main_with_copy.bat
@@ -1,4 +1,4 @@
-xcopy /y data\2024_2\settings.py settings.py
+xcopy /y data\2024_3\settings.py settings.py
 python main.py
-xcopy /y created_tables\standings.html ..\acmallukrainian\2024\2\
+xcopy /y created_tables\standings.html ..\acmallukrainian\2024\3\
 TIMEOUT /T -1
