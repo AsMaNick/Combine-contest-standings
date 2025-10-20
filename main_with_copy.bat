946c4bb49e13b0bab3863015bf582e1b178f35a9
Matvey Aslandukov <matvej909@gmail.com>
2025-10-20 15:02:40 +0200

    Simplified copy script

--- main_with_copy.bat
+++ main_with_copy.bat
@@ -1,4 +1,6 @@
-xcopy /y data\2024_3\settings.py settings.py
+set Season=2025
+set Stage=1
+xcopy /y data\%Season%_%Stage%\settings.py settings.py
 python main.py
-xcopy /y created_tables\standings.html ..\acmallukrainian\2024\3\
+xcopy /y created_tables\standings.html ..\acmallukrainian\%Season%\%Stage%\
 TIMEOUT /T -1
