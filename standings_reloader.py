788e8ac21620f780eccbdbb781e6aad65572630f
Matvii Aslandukov <matvii.aslandukov@gmail.com>
2026-01-27 00:06:59 +0100

    Added index.html to reloader tracker

--- standings_reloader.py
+++ standings_reloader.py
@@ -8,24 +8,27 @@ import subprocess
 from datetime import datetime, timedelta
 
 
-def get_standings_snapshot():
+def get_files_snapshot():
     root_dir = os.path.expanduser(os.path.join(reloader_config['acmallukrainian_path'],
                                                reloader_config['directory_to_monitor']))
-    standings = dict()
+    files = dict()
     for dirpath, dirnames, filenames in os.walk(root_dir):
         for filename in filenames:
-            if filename == 'standings.html':
+            if filename in ['index.html', 'standings.html']:
                 full_path = os.path.join(dirpath, filename)
                 with open(full_path, 'rb') as f:
-                    standings[os.path.relpath(dirpath, root_dir)] = {
+                    relpath = os.path.relpath(dirpath, root_dir)
+                    assert relpath not in files
+                    files[relpath] = {
                         'full_path': full_path,
+                        'filename': filename,
                         'content': f.read()
                     }
-    return standings
+    return files
 
 
 def reload_standings():
-    global old_standings, last_update, total_updates
+    global old_files, last_update, total_updates
     credentials = reloader_config['credentials']
     print(f'{datetime.now().strftime("%d.%m.%Y %H:%M:%S")}, scripts ', end='', flush=True)
     start_time = time.time()
@@ -35,25 +38,26 @@ def reload_standings():
         if return_code:
             raise subprocess.CalledProcessError(return_code, script)
     print(f'{time.time() - start_time:.3f}s, diffs = ', end='', flush=True)
-    new_standings = get_standings_snapshot()
-    diffs = [path for path in new_standings if old_standings.get(path) != new_standings.get(path)]
-    old_standings = new_standings
+    new_files = get_files_snapshot()
+    diffs = [path for path in new_files if old_files.get(path) != new_files.get(path)]
+    old_files = new_files
     if not diffs:
         print('[]', flush=True)
         return
-    total_updates += 1
     print(sorted(diffs), f'after {str(timedelta(seconds=int(time.time() - last_update)))}', end='', flush=True)
     last_update = time.time()
     with ftplib.FTP('s1.ho.ua') as ftp:
         ftp.login(**credentials)
         ftp.cwd('htdocs/' + reloader_config['directory_to_monitor'])
         for path in diffs:
+            total_updates += 1
             try:
                 ftp.mkd(path)
             except (ftplib.error_perm,):
                 pass
-            with open(old_standings[path]['full_path'], 'rb') as f:
-                ftp.storbinary(f'STOR {path}/standings.html', f)
+            with open(new_files[path]['full_path'], 'rb') as f:
+                filename = new_files[path]['filename']
+                ftp.storbinary(f'STOR {path}/{filename}', f)
     print(f', upload #{total_updates} in {time.time() - last_update:.3f}s', flush=True)
 
 
@@ -74,7 +78,7 @@ def handle_sigint(signum, frame):
 signal.signal(signal.SIGINT, handle_sigint)
 stop_flag = threading.Event()
 reloader_config = json.load(open('data/reloader_config.json', 'r'))
-old_standings = get_standings_snapshot()
+old_files = get_files_snapshot()
 last_update = time.time()
 total_updates = 0
 thread = threading.Thread(target=upload_loop)
